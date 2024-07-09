5449039a37815fbd95c30ffc70722452955ac363
francisgobbi <francis.gobbi@db.tec.br>
2024-07-09 16:34:15 -0300

    add tests para conteudos criar, consultar, alterar e deletar

--- test/test-users.test.js
+++ test/test-users.test.js
@@ -2,6 +2,7 @@ const request = require ('supertest');
 const fakerBr = require('faker-br');
 const rota = "http://localhost:3000"
 
+
 describe('Suite de testes da api users..', ()=>{
 
     const cadastro_usuario_faker = {
@@ -74,5 +75,52 @@ describe('Suite de testes da api users..', ()=>{
        
     });
 
+
+});
+
+describe('Suite de testes da api conteudos..', ()=>{
+
+  const cadastro_conteudo_faker = {
+    id: fakerBr.id,
+    titulo: fakers.titulo,
+    descricao: fakerBr.descricao,
+    tipoConteudo: fakerBr.internet.tipoConteudo(),
+    conteudo: fakerBr.internet.conteudo(),
+    dataCadastro: fakerBr.internet.dataCadastro()
+  }
+
+
+it('Cadastrar um novo conteúdo e verificar que o conteúdo está devidamente retornando os dados esperados e o statusCode 201.', async()=> {
+  const response = await request(rota)
+    .post('/conteudos')
+    .send(cadastro_conteudo_faker);         
+     expect(response.status).toBe(201)
+     console.log(response.body)   
+});
+
+it('Realizar a consulta do conteúdo em que acabou de cadastrar, retornando os dados esperados e o statusCode 200.', async()=> {
+  const response = await request(rota)
+    .get('/conteudos/id')  
+    .send(cadastro_conteudo_faker);            
+     expect(response.status).toBe(200)
+     console.log(response.body)
+ 
+});
+
+it('Você deverá alterar o conteúdo consultado anteriormente, e em seguida validar se realmente os dados foram alterados e o statusCode 201.', async()=> {
+  const response = await request(rota)
+    .post('/conteudos')
+    .send(cadastro_conteudo_faker);         
+     expect(response.status).toBe(201)
+     console.log(response.body)   
+});
+
+it('Remover o conteúdo e garantir que o mesmo foi removido, não existe mais para consulta e o statusCode 200.', async()=> {
+  const response = await request(rota)
+    .get('/conteudos/id')             
+     expect(response.status).toBe(200)
+     console.log(response.body)
+ 
+});
 
 });
