7bdab4c4ff3539d8d343dda9a8b44c8649d8cf0f
francisgobbi <francis.gobbi@db.tec.br>
2024-07-12 11:08:00 -0300

    update tests

--- test/test-conteudos.test.js
+++ test/test-conteudos.test.js
@@ -4,7 +4,15 @@ const rota = "http://localhost:3000"
 
 describe('Suite de testes da api conteudos..', ()=>{
 
-  const cadastro_conteudo = {    
+  const cadastroConteudo = {    
+    titulo: "O Fim do Mundo",
+    descricao: "A hsitória do fim do mundo",
+    tipoConteudo: "Ficção",
+    conteudo: "Fim do mundo é um evento futuro hipotético que tem o potencial para prejudicar ou extinguir a humanidade e/ou qualquer outra forma de vida no planeta Terra."
+    
+  }
+
+  const alterarCadastroConteudo = {    
     titulo: "O Fim do Mundo",
     descricao: "A hsitória do fim do mundo",
     tipoConteudo: "Ficção",
@@ -16,7 +24,7 @@ describe('Suite de testes da api conteudos..', ()=>{
 it('Cadastrar um novo conteúdo e verificar que o conteúdo está devidamente retornando os dados esperados e o statusCode 201.', async()=> {
   const response = await request(rota)
     .post('/conteudos')
-    .send(cadastro_conteudo);   
+    .send(cadastroConteudo);   
     expect(response.body).toBeDefined();
     expect(response.body).toHaveProperty('id');
     expect(response.status).toBe(201)
@@ -29,7 +37,7 @@ it('Cadastrar um novo conteúdo e verificar que o conteúdo está devidamente re
 it('Realizar a consulta do conteúdo em que acabou de cadastrar, retornando os dados esperados e o statusCode 200.', async()=> {
   const response = await request(rota)
     .get(`/conteudos/${idConteudo}`)  
-    .send(cadastro_conteudo); 
+    .send(cadastroConteudo); 
     expect(response.body).toBeDefined(); 
     expect(response.body).toHaveProperty('id',idConteudo);          
     expect(response.status).toBe(200)
@@ -40,7 +48,7 @@ it('Realizar a consulta do conteúdo em que acabou de cadastrar, retornando os d
 it('Você deverá alterar o conteúdo consultado anteriormente, e em seguida validar se realmente os dados foram alterados e o statusCode 201.', async()=> {
   const response = await request(rota)
     .post('/conteudos')
-    .send(cadastro_conteudo);  
+    .send(alterarCadastroConteudo);  
     expect(response.body).toBeDefined(); 
     expect(response.body).toHaveProperty('id',idConteudo);        
     expect(response.status).toBe(201)
@@ -49,8 +57,7 @@ it('Você deverá alterar o conteúdo consultado anteriormente, e em seguida val
 
 it('Remover o conteúdo e garantir que o mesmo foi removido, não existe mais para consulta e o statusCode 200.', async()=> {
   const response = await request(rota)
-    .get(`/conteudos/${idConteudo}`)
-    expect(response.body).toHaveProperty('id',idConteudo);             
+    .get(`/conteudos/${idConteudo}`)            
     expect(response.status).toBe(200)
     console.log(response.body)
  
